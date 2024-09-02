ac3c718951ba3060b77d99c2451e570f021390d9
Galo Navarro <anglorvaroa@gmail.com>
2024-09-02 17:37:22 +0200

    Support last-modified
    Signed-off-by: Galo Navarro <anglorvaroa@gmail.com>

--- pkg/condition_age.go
+++ pkg/condition_age.go
@@ -2,8 +2,6 @@ package labeler
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -35,27 +33,4 @@ func AgeCondition(l *Labeler) Condition {
 			return age > ageDuration, nil
 		},
 	}
-}
-
-func parseExtendedDuration(s string) (time.Duration, error) {
-	multiplier := time.Hour * 24 // default to days
-
-	if strings.HasSuffix(s, "w") {
-		multiplier = time.Hour * 24 * 7 // weeks
-		s = strings.TrimSuffix(s, "w")
-	} else if strings.HasSuffix(s, "y") {
-		multiplier = time.Hour * 24 * 365 // years
-		s = strings.TrimSuffix(s, "y")
-	} else if strings.HasSuffix(s, "d") {
-		s = strings.TrimSuffix(s, "d") // days
-	} else {
-		return time.ParseDuration(s) // default to time.ParseDuration for hours, minutes, seconds
-	}
-
-	value, err := strconv.Atoi(s)
-	if err != nil {
-		return 0, err
-	}
-
-	return time.Duration(value) * multiplier, nil
 }
