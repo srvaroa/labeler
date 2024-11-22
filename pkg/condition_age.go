fe4b1c73bb8abf2f14a44a6912a8b4fee835d631
Dave Lunny <4298089+himynameisdave@users.noreply.github.com>
2024-11-22 07:09:41 -0800

    Convert age matcher to use a duration config (#159)
    * 🚀 Allow age with at-least/at-most options
    
    Marks the old `age: 2d` option as deprecated but keeps it for backward compatability

--- pkg/condition_age.go
+++ pkg/condition_age.go
@@ -14,10 +14,35 @@ func AgeCondition(l *Labeler) Condition {
 			return target.ghIssue != nil || target.ghPR != nil
 		},
 		Evaluate: func(target *Target, matcher LabelMatcher) (bool, error) {
-			// Parse the age from the configuration
-			ageDuration, err := parseExtendedDuration(matcher.Age)
-			if err != nil {
-				return false, fmt.Errorf("failed to parse age parameter in configuration: %v", err)
+			// Backward compatibility: If "age" is provided as a string, treat it as "at-least"
+			var atLeastDuration, atMostDuration time.Duration
+			var err error
+
+			//	If they have specified a legacy "age" field, use that
+			//	and treat it is as "at-least"
+			if matcher.Age != "" {
+				atLeastDuration, err = parseExtendedDuration(matcher.Age)
+				if err != nil {
+					return false, fmt.Errorf("failed to parse age parameter in configuration: %v", err)
+				}
+			} else if matcher.AgeRange != nil {
+				// Parse "at-least" if specified
+				if matcher.AgeRange.AtLeast != "" {
+					atLeastDuration, err = parseExtendedDuration(matcher.AgeRange.AtLeast)
+					if err != nil {
+						return false, fmt.Errorf("failed to parse `age.at-least` parameter in configuration: %v", err)
+					}
+				}
+
+				// Parse "at-most" if specified
+				if matcher.AgeRange.AtMost != "" {
+					atMostDuration, err = parseExtendedDuration(matcher.AgeRange.AtMost)
+					if err != nil {
+						return false, fmt.Errorf("failed to parse `age.at-most` parameter in configuration: %v", err)
+					}
+				}
+			} else {
+				return false, fmt.Errorf("no age conditions are set in config")
 			}
 
 			// Determine the creation time of the issue or PR
@@ -30,7 +55,15 @@ func AgeCondition(l *Labeler) Condition {
 
 			age := time.Since(createdAt)
 
-			return age > ageDuration, nil
+			//	Check if the age of the issue/PR is within the specified range
+			if atLeastDuration != 0 && age < atLeastDuration {
+				return false, nil
+			}
+			if atMostDuration != 0 && age > atMostDuration {
+				return false, nil
+			}
+
+			return true, nil
 		},
 	}
 }
